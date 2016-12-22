a4f2fa8d8d85835bb258753e2ed080af7022f18b
Rokkrs <rokkrs@MacBook-Pro-de-Oscar.local>
2016-12-22 12:55:30 -0300

    Initial Commit

---
+++ AllesMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllesMusic
+//
+//  Created by Oscar on 12/22/16.
+//  Copyright © 2016 RokkrsStore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
